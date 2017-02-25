bd173e06f79d96b4f28ab0e8cde37e6616327739
student <student@softeng>
2017-02-24 18:19:45 -0800

    wrote remaining implementations for the function

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -50,13 +50,12 @@ Piece TicTacToeBoard::placePiece(int row, int column)
   if(board[row][column]==Blank)
   {
     board[row][column]=turn;
-    Piece value=turn;
     toggleTurn();
-    return value;
+    return board[row][column];
   }
   else if(board[row][column]==turn)
   {
-    return turn;
+    return board[row][column];
   }
   else
     return Invalid;
@@ -79,9 +78,22 @@ Piece TicTacToeBoard::getWinner()
 {
   for(int i=0;i<BOARDSIZE; i++)
   {
-   if(board[i][0]==board[i][1] && board[i][0]==board[i]][2])
+   if(board[i][0]==board[i][1] && board[i][0]==board[i][2])
    {
      return board[i][0];
    }
+   else if(board[0][i]==board[1][i] && board[1][i] == board[2][i])
+   {
+     return board[1][i];
+   }
   }
+  if(board[0][0] == board[1][1] && board[1][1] == board[2][2])
+  {
+    return board[0][0];
+  }
+  if(board[0][2] == board[1][1] && board[1][1] == board[2][0])
+  {
+    return board[1][1];
+  }
+   return Invalid;
 }
